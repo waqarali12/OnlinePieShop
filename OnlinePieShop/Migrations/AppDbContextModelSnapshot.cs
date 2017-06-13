e39613c19aa52b657ae4870999dd6b9e43fb2003
Waqar Ali Zaidi <waqar.zaidi@valtech.com>
2017-06-13 10:51:25 +0200

    EF Core

---
+++ OnlinePieShop/Migrations/AppDbContextModelSnapshot.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using OnlinePieShop.Models;
+
+namespace OnlinePieShop.Migrations
+{
+    [DbContext(typeof(AppDbContext))]
+    partial class AppDbContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("OnlinePieShop.Models.Category", b =>
+                {
+                    b.Property<int>("CategoryId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("CategoryName");
+
+                    b.Property<string>("Description");
+
+                    b.HasKey("CategoryId");
+
+                    b.ToTable("Categories");
+                });
+
+            modelBuilder.Entity("OnlinePieShop.Models.Pie", b =>
+                {
+                    b.Property<int>("PieId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("AllergyInformation");
+
+                    b.Property<int>("CategoryId");
+
+                    b.Property<string>("ImageThumbnailUrl");
+
+                    b.Property<string>("ImageUrl");
+
+                    b.Property<bool>("InStock");
+
+                    b.Property<bool>("IsPieOfTheWeek");
+
+                    b.Property<string>("LongDescription");
+
+                    b.Property<string>("Name");
+
+                    b.Property<decimal>("Price");
+
+                    b.Property<string>("ShortDescription");
+
+                    b.HasKey("PieId");
+
+                    b.HasIndex("CategoryId");
+
+                    b.ToTable("Pies");
+                });
+
+            modelBuilder.Entity("OnlinePieShop.Models.Pie", b =>
+                {
+                    b.HasOne("OnlinePieShop.Models.Category", "Category")
+                        .WithMany("Pies")
+                        .HasForeignKey("CategoryId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
--- OnlinePieShop/Startup.cs
+++ OnlinePieShop/Startup.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using OnlinePieShop.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace OnlinePieShop
 {
@@ -10,13 +12,23 @@ namespace OnlinePieShop
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        private IConfigurationRoot _ConfigurationRoot;
+        public Startup(IHostingEnvironment hostingEnvironment)
+        {
+            _ConfigurationRoot = new ConfigurationBuilder()
+                .SetBasePath(hostingEnvironment.ContentRootPath)
+                .AddJsonFile("appsettings.json")
+                .Build();
+        }
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options => 
+                                                options.UseSqlServer(_ConfigurationRoot.GetConnectionString("DefaultConnection")));
             //services.AddSingleton--single obj for all sessions
             //services.AddScoped-single object for one session  
             //services.AddTransient new obj whenever called
-            services.AddTransient<IPieRepository, MockPieRepository>(); 
-            services.AddTransient<ICategoryRepository, MockCategoryRepository>();
+            services.AddTransient<IPieRepository, PieRepository>(); 
+            services.AddTransient<ICategoryRepository, CategoryRepository>();
             services.AddMvc();
         }
 
@@ -27,6 +39,8 @@ namespace OnlinePieShop
             app.UseStatusCodePages();
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
+            DbInitializer.Seed(app);
         }
-    }
+    }   
 }
+        
